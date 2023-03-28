95435c77f3b5cb19a5062ec8b4f782319eb01671
Erik Percic <erik.percic@shopify.com>
2023-03-28 01:05:06 -0400

    Add default domain value

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -1,7 +1,7 @@
 module UrlHelper
     def generate_url(relative_path:)
-        domain = ENV['DOMAIN']
-        
+        domain = ENV['DOMAIN'] || "http://localhost:3000"
+
         URI.join(domain, relative_path).to_s
     end
-end 
+end
